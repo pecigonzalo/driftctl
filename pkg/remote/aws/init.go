e5f321ebb1f0fa20e4ba4d88be32485c66a6e720
sundowndev <raphael@crvx.fr>
2021-06-04 18:25:06 +0200

    feat: implement cache in ecr repository

--- pkg/remote/aws/init.go
+++ pkg/remote/aws/init.go
@@ -46,7 +46,7 @@ func Init(alerter *alerter.Alerter,
 	dynamoDBRepository := repository.NewDynamoDBRepository(provider.session)
 	cloudfrontRepository := repository.NewCloudfrontClient(provider.session)
 	kmsRepository := repository.NewKMSRepository(provider.session)
-	ecrRepository := repository.NewECRRepository(provider.session)
+	ecrRepository := repository.NewECRRepository(provider.session, repositoryCache)
 
 	deserializer := resource.NewDeserializer(factory)
 	providerLibrary.AddProvider(terraform.AWS, provider)
